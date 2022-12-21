385f359948ca6df06aae2e84e5e28313ca0fd189
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2022-12-21 11:18:18 +0100

    primer commit

---
+++ 2 Semestre/metodos/src/Coche.java
@@ -0,0 +1,16 @@
+public class Coche {
+    public int velocidad;
+    public int autonomia;
+    private String modelo;
+    private int marcha;
+    private int anguloVolante;
+
+
+    public int acelerar (int velocidadAAcelerar){
+        String nombre = "pepe";
+        int edad = 24;
+
+        this.velocidad = this.velocidad + velocidadAAcelerar;
+        return 0;
+    }
+}
---
+++ 2 Semestre/metodos/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Coche coche = new Coche();
+
+        coche.acelerar(100);
+    }
+}
