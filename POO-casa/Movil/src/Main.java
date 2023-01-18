065119e762cc395130856aeaeacff234c246008c
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-01-18 13:26:37 +0100

    primer commit

---
+++ POO-casa/Movil/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Movil movil = new Movil("Iphone",false,"ios16",true);
+
+        movil.encederMovil();
+
+        System.out.println(movil.toString());
+    }
+}
